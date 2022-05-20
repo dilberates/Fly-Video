9b8f305bbbd5c6c32cdbd0c232de9cfe0d469362
Dilber KILIÇ <dilberklc5252@gmail.com>
2022-05-20 11:58:48 +0300

    update
    Login ve sign arası bağlantı yapıldı. Veritabanı kısmına geçmeye hazırlandı.

--- app/src/main/java/com/example/fly_video/loginActivity.java
+++ app/src/main/java/com/example/fly_video/loginActivity.java
@@ -2,13 +2,36 @@ package com.example.fly_video;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class loginActivity extends AppCompatActivity {
 
+    EditText email,password;
+    Button login,sign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initCompenent();
+
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(loginActivity.this,SignupActivity.class));
+            }
+        });
     }
+
+    private void initCompenent() {
+        email=findViewById(R.id.emailInput);
+        password=findViewById(R.id.passwordInput);
+        login=findViewById(R.id.loginBtn);
+        sign=findViewById(R.id.createBtn);
+
+    }
+
 }
