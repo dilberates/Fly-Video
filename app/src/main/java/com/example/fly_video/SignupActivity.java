a48c358c1c85c9a10a168724615df5cbcdc63859
Dilber KILIÇ <dilberklc5252@gmail.com>
2022-05-25 20:03:43 +0300

    update
    Firebasefirestore hatası çözüldü.

--- app/src/main/java/com/example/fly_video/SignupActivity.java
+++ app/src/main/java/com/example/fly_video/SignupActivity.java
@@ -3,6 +3,7 @@ package com.example.fly_video;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,14 +11,17 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignupActivity extends AppCompatActivity {
 
 
     FirebaseAuth auth;
+    FirebaseFirestore database;
     EditText email,password,name;
     Button login,create;
     @Override
@@ -27,6 +31,7 @@ public class SignupActivity extends AppCompatActivity {
         initCompenent();
 
         auth=FirebaseAuth.getInstance();
+        database=FirebaseFirestore.getInstance();
 
         create.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,10 +41,22 @@ public class SignupActivity extends AppCompatActivity {
                 mpass=password.getText().toString();
                 mname=name.getText().toString();
 
+                Users users=new Users();
+                users.setEmail(memail);
+                users.setName(mname);
+                users.setPassword(mpass);
+
                 auth.createUserWithEmailAndPassword(memail,mpass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
+                            database.collection("Users")
+                                    .document().set(users).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    startActivity(new Intent(SignupActivity.this,loginActivity.class));
+                                }
+                            });
                             Toast.makeText(SignupActivity.this,"Yeni hesap oluşturuldu.",Toast.LENGTH_SHORT).show();
 
                         }else{
