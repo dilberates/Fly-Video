5f8c532ebcb0b67bfc9286c6bf9df89f13a3009b
Dilber KILIÇ <dilberklc5252@gmail.com>
2022-06-05 02:22:28 +0300

    Update

--- app/src/main/java/com/example/fly_video/SettingsFragment.java
+++ app/src/main/java/com/example/fly_video/SettingsFragment.java
@@ -2,22 +2,66 @@ package com.example.fly_video;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.renderscript.ScriptGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.fly_video.databinding.FragmentHistoryBinding;
+import android.widget.Button;
+import android.widget.TextView;
+import com.example.fly_video.databinding.FragmentSettingsBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SettingsFragment extends Fragment {
 
-    private ScriptGroup.Binding binding;
+
+    private FirebaseDatabase database;
+    private DatabaseReference userRef;
+    private FragmentSettingsBinding binding;
+    private FirebaseAuth auth;
+    Button maccounti,minfo,mhelp;
+    TextView name,mail;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding= FragmentSettingsBinding.inflate(inflater,container,false);
+        View root=binding.getRoot();
 
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        maccounti=binding.account;
+        minfo=binding.info;
+        mhelp=binding.help;
+        name=binding.fullNameTxt;
+        mail=binding.emailTxt;
+
+        auth= FirebaseAuth.getInstance();
+        String uid =auth.getCurrentUser().getUid();
+        database=FirebaseDatabase.getInstance("https://flyvideo-a5ed2-default-rtdb.firebaseio.com/");
+        userRef=database.getReference("Users").child(uid);
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String dname = ds.child("name").getValue(String.class);
+                    String dmail = ds.child("password").getValue(String.class);
+                    name.setText(dname);
+                    mail.setText(dmail);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return root;
     }
 }
